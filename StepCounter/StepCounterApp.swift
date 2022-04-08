47265ca92108d1bc8ff922e574124e75c0ab7922
Brian Moriguchi <brian@beckos.com>
2022-04-07 18:12:08 -0700

    Initial Commit

---
+++ StepCounter/StepCounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  StepCounterApp.swift
+//  StepCounter
+//
+//  Created by Tatsuya Moriguchi on 4/7/22.
+//
+
+import SwiftUI
+
+@main
+struct StepCounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
